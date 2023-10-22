680189fc7fd722485c4e4501d618b2815c299771
sf3t <teemu.liimatta@hotmail.com>
2023-10-22 18:31:55 +0300

    Updated scripts.js
    added functionality to drag and move the notes

--- script.js
+++ script.js
@@ -38,4 +38,56 @@ button.addEventListener("click", function() {
   postIt.style.left = x + "px";
   postIt.style.top = y + "px";
   postIt.style.transform = "rotate(" + r + "deg)";
+
+  // Add event listeners for drag and drop functionality
+  postIt.addEventListener("mousedown", startDrag);
+  postIt.addEventListener("mousemove", moveDrag);
+  postIt.addEventListener("mouseup", stopDrag);
 });
+
+// Variables to store the state and the offset of the drag
+var dragging = false;
+var offsetX = 0;
+var offsetY = 0;
+
+// Function to start the drag when the mouse is pressed on a post-it note
+function startDrag(e) {
+  // Set the dragging flag to true
+  dragging = true;
+
+  // Get the current mouse position
+  var mouseX = e.clientX;
+  var mouseY = e.clientY;
+
+  // Get the current position of the post-it note
+  var postItX = parseInt(e.target.style.left);
+  var postItY = parseInt(e.target.style.top);
+
+  // Calculate the offset between the mouse and the post-it note
+  offsetX = mouseX - postItX;
+  offsetY = mouseY - postItY;
+}
+
+// Function to move the post-it note when the mouse is moved while dragging
+function moveDrag(e) {
+  // Check if the dragging flag is true
+  if (dragging) {
+    // Get the current mouse position
+    var mouseX = e.clientX;
+    var mouseY = e.clientY;
+
+    // Calculate the new position of the post-it note
+    var postItX = mouseX - offsetX;
+    var postItY = mouseY - offsetY;
+
+    // Set the new position of the post-it note
+    e.target.style.left = postItX + "px";
+    e.target.style.top = postItY + "px";
+  }
+}
+
+// Function to stop the drag when the mouse is released
+function stopDrag(e) {
+  // Set the dragging flag to false
+  dragging = false;
+}
